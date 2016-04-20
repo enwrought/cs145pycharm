a16eb2bd19bc91bd0c6ad473ec5c2514ff2e1a31
Bryant Lin <bjlin@caltech.edu>
2016-04-20 05:23:25 -0700

    Changed PCA into a class used by the Network class.

--- Network.py
+++ Network.py
@@ -2,10 +2,12 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 import networkx as nx
+import featurePCA
+
 
 class Network:
     """
-        Stub for Netork class
+        Stub for Network class
         
         networkx_obj = NetworkX graph
         features = {featurenames: [network_ids with true as the features]}
@@ -14,9 +16,9 @@ class Network:
             (by default uncomputed as {})
             (indexed by network edge object edges)
     """
-    def __init__(self, featureVec, featuresByName, networkx_obj, all_pairs_edges = {}):
-        self.featureVec = featureVec
-        self.featureByName = featuresByName
+    def __init__(self, feature_vec, features_by_name, networkx_obj, all_pairs_edges={}):
+        self.feature_vec = feature_vec
+        self.feature_by_name = features_by_name
         self.networkx_obj = networkx_obj
         self.all_pairs_edges = all_pairs_edges
-    
+        self.pca = featurePCA.FeaturePCA(features=feature_vec, featureIDs=features_by_name)
--- featurePCA.py
+++ featurePCA.py
@@ -1,16 +1,34 @@
-import Network
 import numpy as np
+import math
 from sklearn.decomposition import PCA
 
-def filterByIndex(user, featureIDs):
-    return [user[i] for i in featureIDs]
 
-# Inputs a network and a list of relevant feature ID's
-def featurePCA(network, featureIDs):
+class FeaturePCA:
+    def __init__(self, features, featureIDs):
+        """
+            @:param{features}{list} - Network.feature_vec object
+            @:param{featureIDs}{list} - List of feature IDs
 
-    featureList = network.features
+            Initializes and combines the PCA.
+        """
+        # TODO: add parameter to allow passing advanced options to PCA
+        def filterByIndex(user, featureIDs):
+            return [user[i] for i in featureIDs]
 
-    filteredList = [filterByIndex(user, featureIDs) for user in featureList]
+        featureList = features
+        filteredList = [filterByIndex(user, featureIDs) for user in featureList]
+        self.pca = PCA(n_components=len(featureIDs))
+        self.pca.fit(filteredList)
 
-    pca = PCA(n_components=len(featureIDs))
-    pca.fit(filteredList)
+    def generate_PCA_graph(self, filename):
+        """
+            Creates the graph of the points in PCA space and saves to 'filename'
+        """
+        pass
+
+    def get_distances(self, metric=lambda coord1, coord2: math.sqrt((coord1-coord2)*(coord1-coord2))):
+        """
+            Returns a list of distances (in PCA space)
+            @:param{metric}{function} - Metric for distances between two points (default Euclidean)
+        """
+        pass
